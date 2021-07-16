2b07cbf31d5d48d2218f71f88d6e9e4e9c431158
Kevin Daniel Sanchez <Dsanchez0397@gmail.com>
2021-07-16 12:01:52 -0500

    Extends: User model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f'Profile for user {self.user.username}'
 
 # Create your models here.
