bda954fc205f63b4630c688fa303a8d027b6dc1e
Kevin Daniel Sanchez <Dsanchez0397@gmail.com>
2021-07-19 11:35:15 -0500

    🔧 Add family model to admin page

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Family, Membership
+
+class MembershipInline(admin.TabularInline):
+    model = Membership
+    extra = 1
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
+    inlines = [MembershipInline]
     list_display = ['user', 'date_of_birth']
+
+@admin.register(Family)
+class FamilyAdmin(admin.ModelAdmin):
+    inlines = [MembershipInline]
+    list_display = ['name']
