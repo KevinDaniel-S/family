daab8159f6e9d2f01cbdf614ee8d79b9f1f7bcaf
Kevin Daniel Sanchez <Dsanchez0397@gmail.com>
2021-07-19 19:29:56 -0500

    🔨 Add validations for register and edit user

--- account/forms.py
+++ account/forms.py
@@ -12,9 +12,24 @@ class UserRegistrationForm(forms.ModelForm):
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
 
+    def clean_username(self):
+        cd = self.cleaned_data
+        user = cd['username'].capitalize()
+        if User.objects.filter(username=user).exists():
+            raise forms.ValidationError('User already exists')
+        return user
+
+    def clean_email(self):
+        cd = self.cleaned_data
+        email = cd['email'].capitalize()
+        if email == "":
+            return email
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('E-mail already exists')
+        return email
+
     def clean_password2(self):
         cd = self.cleaned_data
-        print(cd)
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
@@ -23,10 +38,23 @@ class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
+    def clean_username(self):
+        cd = self.cleaned_data
+        return cd['username'].capitalize()
+
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email')
+
+    def clean_email(self):
+        cd = self.cleaned_data
+        email = cd['email'].capitalize()
+        if email=='' or self.instance.email==email:
+            return email
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('E-mail already exists')
+        return email
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
