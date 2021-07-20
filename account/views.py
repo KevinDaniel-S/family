228087313c3a9e3483fa348c3ee8ddf8d0821168
Kevin Daniel Sanchez <Dsanchez0397@gmail.com>
2021-07-19 19:33:27 -0500

    :coffin: Remove old login view

--- account/views.py
+++ account/views.py
@@ -7,27 +7,6 @@ from .forms import LoginForm, UserRegistrationForm, \
                    UserEditForm, ProfileEditForm
 from .models import Profile
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Authenticated '\
-                                        'succesfully')
-                else:
-                    return HttpResponse('Disabled account')
-            else:
-                return HttpResponse('Invalid login')
-    else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form':form})
-
 @login_required
 def dashboard(request):
     return render(request, 
@@ -37,7 +16,6 @@ def dashboard(request):
 def register(request):
     if request.method=='POST':
         user_form = UserRegistrationForm(request.POST)
-        print(request.POST)
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
             new_user.set_password(
