3aa0e99892df98f4344e50d63b7cca2d07ab8286
Kevin Daniel Sanchez <Dsanchez0397@gmail.com>
2021-07-20 18:30:33 -0500

    :sparkles: Create family view

--- household/views.py
+++ household/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Family, Membership
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def family(request) -> render:
+    profile = request.user.profile
+    
+    households = profile.family_set.all()
+    if len(households)==0:
+        household = None
+    else: 
+        household = households[0]
+    
+    return render(request, 'family.html', 
+                 {'section':'family', 'family':household})
