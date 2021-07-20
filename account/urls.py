c6e4da33fa1f4d86f244afd0ac6918023beefc2e
Kevin Daniel Sanchez <Dsanchez0397@gmail.com>
2021-07-19 19:31:12 -0500

    :children_crossing: Add alias url to dashboard

--- account/urls.py
+++ account/urls.py
@@ -5,10 +5,11 @@ from . import views
 urlpatterns = [
         path('register/', views.register, name='register'),
 
-        path('login/',auth_views.LoginView.as_view(), name='login'),
+        path('login/', auth_views.LoginView.as_view(), name='login'),
         path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
         path('dashboard/', views.dashboard, name="dashboard"),
+        path('', views.dashboard, name="dashboard"),
 
         path('password_change/', auth_views.PasswordChangeView.as_view(),
              name='password_change'),
